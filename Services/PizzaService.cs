297fa2b18f91c9832056dfdad60176efb9691789
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-15 10:32:10 -0300

    add pizzas

--- Services/PizzaService.cs
+++ Services/PizzaService.cs
@@ -19,4 +19,10 @@ public static class PizzaService
     public static List<Pizza> GetAll() => Pizzas;
 
     public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
 }
